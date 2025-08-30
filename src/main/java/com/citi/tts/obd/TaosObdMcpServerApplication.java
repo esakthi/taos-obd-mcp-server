612a763dbf7564bac83fe9dfe7eefddf4e7a4fa8
Sakthivadivel Easwaramoorthy <sakthi.nem@gmail.com>
2025-08-30 20:17:22 +0530

    Add files via upload

---
+++ src/main/java/com/citi/tts/obd/TaosObdMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.citi.tts.obd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaosObdMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaosObdMcpServerApplication.class, args);
+	}
+
+}
